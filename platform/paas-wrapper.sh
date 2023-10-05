1fce664deb1579fbb8e1d0a2424fac4270e74886
Michal Charemza <michal@charemza.name>
2023-10-05 12:28:06 +0100

    fix: set DAGSTER_HOME in PaaS, which the daemon needs

--- platform/paas-wrapper.sh
+++ platform/paas-wrapper.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+
 export POSTGRES_URL=$(echo $VCAP_SERVICES | jq -r '.postgres[] | select (.instance_name == "data-flow-2-db").credentials.uri'| sed "s/^postgres/postgresql/")
+export DAGSTER_HOME=/home/vcap/app
 
 exec "$@"
