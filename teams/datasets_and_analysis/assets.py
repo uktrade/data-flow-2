5faadb4f62a182a409e40e360246242e6870c66c
Michal Charemza <michal@charemza.name>
2023-10-05 08:59:31 +0100

    feat: add some fake dependencies

--- teams/datasets_and_analysis/assets.py
+++ teams/datasets_and_analysis/assets.py
@@ -1,5 +1,13 @@
-from dagster import asset
+from dagster import AssetKey, Definitions, SourceAsset, asset
 
-@asset
+predictions_asset = SourceAsset(key=AssetKey("predictions"))
+licenses_asset = SourceAsset(key=AssetKey("licences"))
+supply_chains_assets = SourceAsset(key=AssetKey("supply_chains"))
+
+@asset(deps=[
+    predictions_asset,
+    licenses_asset,
+    supply_chains_assets,
+])
 def interactions() -> None:
     print('Fetching interactions')
