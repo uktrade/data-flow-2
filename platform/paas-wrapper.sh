4bd6b17d6886629bff4a08ae3c2658e370adb591
Michal Charemza <michal@charemza.name>
2023-10-05 15:58:43 +0100

    fix: celery SSL and wrapping working in correct environment variables

--- platform/paas-wrapper.sh
+++ platform/paas-wrapper.sh
@@ -3,7 +3,7 @@
 set -e
 
 export POSTGRES_URL=$(echo $VCAP_SERVICES | jq -r '.postgres[] | select (.instance_name == "data-flow-2-db").credentials.uri' | sed "s/^postgres/postgresql/")
-export REDIS_URL=$(echo $VCAP_SERVICES | jq -r '.redis[] | select (.instance_name == "data-flow-2-redis").credentials.uri')
+export REDIS_URL=$(echo $VCAP_SERVICES | jq -r '.redis[] | select (.instance_name == "data-flow-2-redis").credentials.uri')'?ssl_cert_reqs=CERT_REQUIRED&ssl_ca_certs=/etc/ssl/certs/ca-certificates.crt'
 export DAGSTER_HOME=/home/vcap/app
 
 exec "$@"
