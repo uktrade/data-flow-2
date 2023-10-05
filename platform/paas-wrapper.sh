2540658d7aad20d37dd3aff5540d2083e1d7828a
Michal Charemza <michal@charemza.name>
2023-10-05 18:29:08 +0100

    feat: separate code location for DEET in PaaS

--- platform/paas-wrapper.sh
+++ platform/paas-wrapper.sh
@@ -6,4 +6,7 @@ export POSTGRES_URL=$(echo $VCAP_SERVICES | jq -r '.postgres[] | select (.instan
 export REDIS_URL=$(echo $VCAP_SERVICES | jq -r '.redis[] | select (.instance_name == "data-flow-2-redis").credentials.uri')'?ssl_cert_reqs=CERT_REQUIRED&ssl_ca_certs=/etc/ssl/certs/ca-certificates.crt'
 export DAGSTER_HOME=/home/vcap/app
 
+# Would need to be different for prod
+export CODE_SERVERS__DEET__HOST=data-flow-2-code-server-deet-staging.apps.internal
+
 exec "$@"
