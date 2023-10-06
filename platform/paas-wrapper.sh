7729ec6a0d9b36f40c9111c5f8f06b47cb5a5f6b
Michal Charemza <michal@charemza.name>
2023-10-06 09:28:20 +0100

    refactor: tidy file locations to make responsibility clearer

--- platform/paas-wrapper.sh
+++ platform/paas-wrapper.sh
@@ -4,7 +4,7 @@ set -e
 
 export POSTGRES_URL=$(echo $VCAP_SERVICES | jq -r '.postgres[] | select (.instance_name == "data-flow-2-db").credentials.uri' | sed "s/^postgres/postgresql/")
 export REDIS_URL=$(echo $VCAP_SERVICES | jq -r '.redis[] | select (.instance_name == "data-flow-2-redis").credentials.uri')'?ssl_cert_reqs=CERT_REQUIRED&ssl_ca_certs=/etc/ssl/certs/ca-certificates.crt'
-export DAGSTER_HOME=/home/vcap/app
+export DAGSTER_HOME=/home/vcap/app/platform
 
 # Would need to be different for prod
 export CODE_SERVERS__DEET__HOST=data-flow-2-code-server-deet-staging.apps.internal
