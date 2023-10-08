e80de0d17dd038bc7d1c1143f2c5911e38680cb2
Michal Charemza <michal@charemza.name>
2023-10-08 11:31:54 +0100

    feat: richer dependency graph

--- teams/datasets_and_analysis/assets.py
+++ teams/datasets_and_analysis/assets.py
@@ -1,19 +1,23 @@
-from dagster import AssetKey, Definitions, SourceAsset, asset, get_dagster_logger
+from dagster import Definitions, asset, get_dagster_logger
 
 logger = get_dagster_logger()
 
-predictions_asset = SourceAsset(key=AssetKey("predictions"))
-licenses_asset = SourceAsset(key=AssetKey("licences"))
-supply_chains_assets = SourceAsset(key=AssetKey("supply_chains"))
-
 
 @asset(deps=[
-    predictions_asset,
-    licenses_asset,
-    supply_chains_assets,
+    "predictions",
+    "licences",
+    "supply_chains",
 ], op_tags={
     'launcher/memory_in_mb': 10000,
     'launcher/disk_in_mb': 500,
 })
 def interactions() -> None:
+    logger.info('Fetching interactions')
+
+
+@asset(deps=[
+    "interactions",
+    "dbt__commodity_codes",
+])
+def report() -> None:
     logger.info('Fetching interactions')
