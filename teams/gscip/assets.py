9cd29828cb0538bbef764fa241ac5125ecbc6b71
Michal Charemza <michal@charemza.name>
2023-10-09 16:51:27 +0100

    feat: enable auto materialise for an asset

--- teams/gscip/assets.py
+++ teams/gscip/assets.py
@@ -1,8 +1,17 @@
-from dagster import asset, get_dagster_logger
+from dagster import asset, get_dagster_logger, AutoMaterializePolicy, AutoMaterializeRule, FreshnessPolicy
 
 logger = get_dagster_logger()
 
 
-@asset
+@asset(
+    auto_materialize_policy=AutoMaterializePolicy.eager().with_rules(
+        AutoMaterializeRule.materialize_on_required_for_freshness(),
+    ),
+    freshness_policy=FreshnessPolicy(
+        maximum_lag_minutes=2,
+        cron_schedule='5/1 * * * *',
+        cron_schedule_timezone='Europe/London',
+    ),
+)
 def supply_chains() -> None:
     logger.info('Ingesting supply chains')
