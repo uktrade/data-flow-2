ff9fa1f63d7fdd6c07cb49791b3adbd75462626d
Michal Charemza <michal@charemza.name>
2023-10-10 11:34:50 +0100

    feat: more checks on licenses

--- teams/lite/assets.py
+++ teams/lite/assets.py
@@ -1,8 +1,25 @@
-from dagster import asset, get_dagster_logger
+from dagster import asset, get_dagster_logger, AssetCheckSpec, AssetCheckResult, AssetCheckSeverity, Output
 
 logger = get_dagster_logger()
 
 
-@asset
+@asset(check_specs=[
+    AssetCheckSpec(name="has_no_nulls", asset="licences"),
+    AssetCheckSpec(name="has_some_minor_issue", asset="licences"),
+])
 def licences() -> None:
+    yield AssetCheckResult(
+        check_name="has_no_nulls",
+        passed=False
+    )
+    yield AssetCheckResult(
+        check_name="has_some_minor_issue",
+        severity=AssetCheckSeverity.WARN,
+        passed=False,
+        metadata={
+            "number_of_rows_with_issue": int(10),
+        },
+    )
+
     logger.info('Fetching licences')
+    yield Output(value=[])
