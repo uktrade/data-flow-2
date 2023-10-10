9d084b05ac403bd4c04456b55fd7966ba00f077a
Michal Charemza <michal@charemza.name>
2023-10-10 09:37:35 +0100

    feat: multiple separate asset checks

--- teams/data_science/__init__.py
+++ teams/data_science/__init__.py
@@ -6,4 +6,9 @@ all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    asset_checks=[
+        assets.my_asset_has_something_1,
+        assets.my_asset_has_something_2,
+        assets.my_asset_has_something_3,
+    ],
 )
--- teams/data_science/assets.py
+++ teams/data_science/assets.py
@@ -1,4 +1,4 @@
-from dagster import asset, get_dagster_logger
+from dagster import asset, asset_check, get_dagster_logger, AssetCheckResult, AssetCheckSeverity
 
 logger = get_dagster_logger()
 
@@ -6,3 +6,18 @@ logger = get_dagster_logger()
 @asset
 def predictions() -> None:
     logger.info('Predicting')
+
+
+@asset_check(asset=predictions, description="Check that the predictions make sense")
+def my_asset_has_something_1() -> AssetCheckResult:
+    return AssetCheckResult(passed=True, metadata={"num_rows": 1253}, severity=AssetCheckSeverity.ERROR)
+
+
+@asset_check(asset=predictions, description="Check that the predictions make sense")
+def my_asset_has_something_2() -> AssetCheckResult:
+    return AssetCheckResult(passed=False, metadata={"num_rows": 21}, severity=AssetCheckSeverity.ERROR)
+
+
+@asset_check(asset=predictions, description="Check that the predictions make sense")
+def my_asset_has_something_3() -> AssetCheckResult:
+    return AssetCheckResult(passed=False, metadata={"num_rows": 42}, severity=AssetCheckSeverity.WARN)
